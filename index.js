314826a5e2ddb3c4d5228703e0cc3f59d3ebbb92
Glitch (maeligg-markov-lyrics-bot) <none>
2020-03-14 15:46:34 +0000

    🌪🌑 Checkpoint ./index.js:1294460/1998

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Twit = require('twit');
-const express = require('express')
+const express = require('express');
+const axios = require('axios');
 
 const app = express()
 app.use(express.static('public'));
@@ -11,8 +12,27 @@ const T = new Twit({
 	access_token_secret: process.env.access_token_secret
 });
 
-app.all('/post', async (req, res) => {
+const searchUrl = 'https://www.googleapis.com/customsearch/v1?q=%22j%27ai%20ajout%C3%A9%22%7C%22j%27ai%20rajout%C3%A9%22%7C%22j%27ai%20enlev%C3%A9%22%7C%22j%27ai%20chang%C3%A9%22%7C%22j%27ai%20modifi%C3%A9%22&cx=014769181873588812818%3Agixypzez3dx&key=AIzaSyBg2sX6awj4WfTQglFRdk9JfCvg3MvNv_Q'
 
+app.all('/post', async (req, res) => {
+  axios.get('https://www.googleapis.com/customsearch/v1', {
+    params: {
+      key: "AIzaSyBg2sX6awj4WfTQglFRdk9JfCvg3MvNv_Q",
+      q: "\"j'ai ajouté\"|\"j'ai rajouté\"|\"j'ai enlevé\"|\"j'ai changé\"|\"j'ai modifié\"",
+      cx: "014769181873588812818:gixypzez3dx"
+    }
+  })
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
 
   
   console.log('songs data fetched successfully');
